0caadbf10654726ea58756dab5cd14628eb4cad6
matija <matija.jeras64@gmail.com>
2021-01-08 00:09:03 +0100

    change to everything embed

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
         <div class="flex w-screen h-screen">
             <div id="twitch-embed" class="w-full"></div>
-            <div id="chat-embed">
-                <iframe frameborder="0"
-                        scrolling="yes"
-                        src="https://www.twitch.tv/embed/xqcow/chat?parent=straemly.netlify.app"
-                        height="100%"
-                        width="340">
-                </iframe>
-            </div>
         </div>
 </template>
 
@@ -19,7 +11,6 @@ export default {
             width: "100%",
             height: "100%",
             channel: 'xqcow',
-            layout: 'video'
             // only needed if your site is also embedded on embed.example.com and othersite.example.com 
             // parent: ["embed.example.com", "othersite.example.com"]
         });
@@ -28,47 +19,5 @@ export default {
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
